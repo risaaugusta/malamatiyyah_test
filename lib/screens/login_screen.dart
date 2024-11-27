0d079d89dadb2279a8dde6c2990bf3c797f12b96
risaaugusta <augustariris20@gmail.com>
2024-11-27 13:36:16 +0700

    add design system components

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text('Malamatiyyah', style: TextStyle(fontFamily:),),
+          Text('Islamic app for your daily faith booster'),
+          
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/style/style.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Coloring{
+  static const  primary = Color(0xff588B76);
+  static const  secondary =  Color(0xffC6D8CF); 
+}
+
+class Fonts{ 
+  static const POPPINS = 'Poppins';
+}

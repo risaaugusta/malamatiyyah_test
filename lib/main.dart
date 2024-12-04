8ee67a10cfcbe6bca9052169ac4f07275aadfcc2
risaaugusta <augustariris20@gmail.com>
2024-12-04 10:16:06 +0700

    update readme

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,17 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:r_muslim/screens/google_login_draft.dart';
 import 'package:r_muslim/screens/login_screen.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp(
+  //     options: FirebaseOptions(
+  //         apiKey: '${dotenv.env['API_KEY']}',
+  //         appId: '${dotenv.env['APP_ID']}',
+  //         messagingSenderId: '${dotenv.env['MESSAGING_SENDER_ID']}',
+  //         projectId: '${dotenv.env['API_KEY']}'));
   await Firebase.initializeApp(
       options: const FirebaseOptions(
           apiKey: 'AIzaSyCeMmpU6EFQHdYlOPY7pkB2ugLuJQG6x3I',
